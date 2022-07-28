cf75b0474235bf62ff15241885238ba0462f2478
Thokozani <66553292+MxoDevops@users.noreply.github.com>
2022-07-28 17:45:26 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import {\n Store, displayTodo, createTodo, editTodos \n} from './crudOps.js';
+import { \nStore, displayTodo, createTodo, editTodos \n} from './crudOps.js';
 import { updateStatus, clearCompleted } from './status.js';
 import './styel.css';
 
